4282d9bc0fd7c98c51dc1f17cd73bcbcdb5af714
mariag5490 <92363619+mariag5490@users.noreply.github.com>
2021-10-12 22:56:00 +0530

    first commit

--- files/main.py
+++ files/main.py
@@ -1,7 +1,37 @@
+
+
 class Bill:
-    def __init__(self, amount):
+    """
+    Object Bill contains amount and period as attributes
+    """
+    def __init__(self,amount,period):
         self.amount=amount
+        self.period=period
+
+class Flatmate:
+    """
+    Person who lives in the flat
+    """
+    def __init__(self,name,days_in_house):
+        self.name=name
+        self.days_in_house=days_in_house
+
+    def pays(self,bill,f2):
+        weight= self.days_in_house/(self.days_in_house+f2.days_in_house)
+        return weight*bill.amount
 
 
-bill=Bill(80)
-print (bill.amount)
+class PdfReport:
+    """
+    Creates pdf file with the name and share of the inmates in the flat
+    """
+    def __init__(self,filename):
+        self.filename=filename
+
+    def generate(self,flatmate1,flatmate2,bill):
+        pass
+bill = Bill(amount=120, period="March 2021")
+harry=Flatmate("Harry",20)
+mary=Flatmate("Mary",25)
+print (harry.pays(bill,mary))
+print(mary.pays(bill,harry))
