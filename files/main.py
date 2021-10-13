b6a969316c5c718d5d17c3f2dc374b2102a86f40
mariag5490 <92363619+mariag5490@users.noreply.github.com>
2021-10-13 23:50:34 +0530

    first commit

--- files/main.py
+++ files/main.py
@@ -1,4 +1,4 @@
-
+from fpdf import FPDF
 
 class Bill:
     """
@@ -29,9 +29,32 @@ class PdfReport:
         self.filename=filename
 
     def generate(self,flatmate1,flatmate2,bill):
-        pass
-bill = Bill(amount=120, period="March 2021")
+        pdf = FPDF(orientation='P', unit='pt', format="A4")
+        pdf.add_page()
+
+        #add title
+        pdf.set_font(family='Times',style='B',size=24)
+        pdf.cell(w=0,h=80,txt="Flatmates Bill",ln=1,border=0)
+
+        #add period
+        pdf.cell(w=100, h=20, txt='Period', border=1, align='L')
+        pdf.cell(w=150, h=20, txt=bill.period, border=1, align='L',ln=1)
+
+        #add flatemate1
+        pdf.cell(w=100, h=20, txt=flatmate1.name, border=1, align='L')
+        pdf.cell(w=200, h=20, txt=str(flatmate1.pays(bill,mary)), border=1, align='L',ln=1)
+
+        # add flatemate2
+        pdf.cell(w=100, h=20, txt=flatmate2.name, border=1, align='L')
+        pdf.cell(w=200, h=20, txt=str(flatmate2.pays(bill, harry)), border=1, align='L',ln=1 )
+
+        pdf.output(self.filename+'.pdf')
+
+
+
+bill = Bill(amount=120, period="March_2021")
 harry=Flatmate("Harry",20)
 mary=Flatmate("Mary",25)
-print (harry.pays(bill,mary))
-print(mary.pays(bill,harry))
+
+report=PdfReport(bill.period)
+report.generate(harry,mary,bill)
